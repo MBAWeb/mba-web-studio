36fb043164099aac649f8cf6a77b7ac63b90f610
MBAWeb <mbashak07@proton.me>
2026-01-14 21:51:12 -0500

    added vercel json file and did polishing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import Home from "./pages/Home";
 import Work from "./pages/Work";
@@ -16,6 +16,22 @@ export default function App() {
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/work/:slug" element={<CaseStudy />} />
+          <Route
+            path="*"
+              element={
+                <div className="containerPage">
+                  <section className="sectionPad">
+                    <h1 className="h1">Page not found</h1>
+                    <p className="p subtle mt-2">
+                      That page doesn’t exist. Try heading back home.
+                    </p>
+                    <Link to="/" className="btn btnPrimary mt-4 inline-block">
+                      Go home
+                    </Link>
+                  </section>
+                </div>
+              }
+            />
         </Routes>
       </Layout>
     </BrowserRouter>
